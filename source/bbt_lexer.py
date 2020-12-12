c14da6973553d7d5a17fa9c2ebe980a12c1c6890
Zboi7667 <zanderbaker100@gmail.com>
2020-12-12 12:52:41 -0800

    Minor wording and syntax changes

--- source/bbt_lexer.py
+++ source/bbt_lexer.py
@@ -37,7 +37,7 @@ tokens = [
 'FUNCTION',       # func
 'PROCESS',         # proc
 
-'COMMENT',  ## --
+'COMMENT',  # ##
 
 ]
 
@@ -115,12 +115,12 @@ data = '''
 [25/(3*40) + {300-20} -16.5]
 {(300-250)<(400-500)}
 20 & 30 | 50
-## This is a comment
+## This is a single-line comment
 '''
 
 # Give the lexer some input
 lexer.input(data)
 
 # Tokenize
-for tok in lexer:
-    print(tok)
+for token in lexer:
+    print(token)
